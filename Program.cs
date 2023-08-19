6340561669be39d9f2a66bf6b2b5940b5adc70f2
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-19 10:03:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArvotJaLaskutoimitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pyydetään käyttäjältä x ja y arvot
+            Console.Write("Syötä x-arvo: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä y-arvo: ");
+            int y = int.Parse(Console.ReadLine());
+
+            // Suoritetaan laskutoimitus x *= y*5
+            x *= y * 5;
+
+            // Tulostetaan tulos
+            Console.WriteLine("x = " + x);
+        }
+    }
+}
